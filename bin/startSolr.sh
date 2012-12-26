c0acd56bd7f11531fd01d9cc028aaf5efb97a329
Benedict Dsilva <bdsilva@scandilabs.com>
2012-12-26 09:27:24 -0500

    copy local schema.xml before starting solr

--- bin/startSolr.sh
+++ bin/startSolr.sh
@@ -1,1 +1,7 @@
+SOLR_LOCAL_HOME=/catamaran/servers/soldonsunday-web/solr_local
+PROJECT_HOME=/projects/soldonsunday/trunk
+
+echo "copying the latest solr schema.xml"
+cp -rf $PROJECT_HOME/src/main/resources/solr/schema.xml $SOLR_LOCAL_HOME/conf/.
+
 /catamaran/servers/soldonsunday-web/solr_tomcat/bin/catalina.sh start
